a78fe1350b66d3e155d63532e4ea95161e531da4
Giorgos Melanis <103131303+giorgosmelanis@users.noreply.github.com>
2025-05-07 01:46:26 +0300

    Revert "Code push"

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys  # Add this import statement
+from PyQt5.QtWidgets import QApplication
+from ui import EventManagementApp
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)  # Now sys.argv is defined
+    window = EventManagementApp()
+    window.show()
+    sys.exit(app.exec_())
